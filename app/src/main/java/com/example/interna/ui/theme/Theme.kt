a11c04f33e4e44c342d02453358c4a7c40129ab1
Art <lyndonehemplo1@gmail.com>
2025-09-20 20:38:28 +0800

    ui theme update

--- app/src/main/java/com/example/interna/ui/theme/Theme.kt
+++ app/src/main/java/com/example/interna/ui/theme/Theme.kt
@@ -12,15 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = calm_white,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = grey,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = blue_green,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = calm_white,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +39,7 @@ private val LightColorScheme = lightColorScheme(
 fun InternaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
