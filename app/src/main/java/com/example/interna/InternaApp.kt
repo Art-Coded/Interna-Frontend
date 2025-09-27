21d64957fd8437dec66db00bf774e3c4e7f5ed4c
Art <lyndonehemplo1@gmail.com>
2025-09-27 21:37:11 +0800

    Hilt + firebase module for backend setup

---
+++ app/src/main/java/com/example/interna/InternaApp.kt
@@ -0,0 +1,9 @@
+package com.example.interna
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InternaApp : Application()
+
+//FOR HILT. LEAVE IT BLANK RAWR
